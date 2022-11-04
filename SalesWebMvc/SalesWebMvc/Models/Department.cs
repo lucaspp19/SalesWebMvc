019d30b8f913c97fd1969cd3441ab0871f31f2cf
lucaspp19 <lucaspp191@gmail.com>
2022-11-04 15:14:58 -0300

    MySQL adaptation and first migration

--- SalesWebMvc/SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/SalesWebMvc/Models/Department.cs
@@ -10,10 +10,6 @@ namespace SalesWebMvc.Models
         public int Id { get; set; }
         public  string Name { get; set; }
 
-        public Department(int id, string name)
-        {
-            Id = id;
-            Name = name;
-        }
+        
     }
 }
