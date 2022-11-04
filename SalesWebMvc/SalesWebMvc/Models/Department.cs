393be866560e28bfcdeb52f0e849bb42d68debba
lucaspp19 <lucaspp191@gmail.com>
2022-11-04 13:37:28 -0300

    CRUD Scaffolding

--- SalesWebMvc/SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/SalesWebMvc/Models/Department.cs
@@ -7,5 +7,13 @@ namespace SalesWebMvc.Models
 {
     public class Department
     {
+        public int Id { get; set; }
+        public  string Name { get; set; }
+
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
     }
 }
