a66ee0db29243ee6b4be61fe0ffd010b1a7b1341
lucaspp19 <lucaspp191@gmail.com>
2022-11-05 11:23:36 -0300

    Seeding Service

--- SalesWebMvc/SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/SalesWebMvc/Models/Seller.cs
@@ -5,44 +5,80 @@ using System.Linq;
 
 
 namespace SalesWebMvc.Models
+
 {
-    public class Seller
+
+   public class Seller
+
     {
+
         public int Id { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
         public DateTime BirthData { get; set; }
         public double BaseSalary { get; set; }
         public Department Department { get; set; }
-        public ICollection<SalesRecord> sales = new List<SalesRecord>();
+        
+        public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
+
+
 
         public Seller()
+
         {
 
         }
 
-        public Seller(int id, string name, string email, DateTime birthData, double baseSalary, Department department)
+
+
+        public Seller(int id, string name, string email, DateTime birthDate, double baseSalary, Department department)
+
         {
+
             Id = id;
+
             Name = name;
+
             Email = email;
-            BirthData = birthData;
+
+            BirthData = birthDate;
+
             BaseSalary = baseSalary;
+
             Department = department;
+
         }
+
+
+
         public void AddSales(SalesRecord sr)
+
         {
-            sales.Add(sr);
+
+            Sales.Add(sr);
+
         }
+
+
+
         public void RemoveSales(SalesRecord sr)
-        {
-            sales.Remove(sr);
-        }
-        public double TotalSales1(DateTime initial, DateTime final)
+
         {
 
-            return sales.Where(sr => sr.Date >= initial && sr.Date <= final).Sum(sr => sr.Amount);
+            Sales.Remove(sr);
 
         }
+
+
+
+        public double TotalSales(DateTime initial, DateTime final)
+
+        {
+
+            return Sales.Where(sr => sr.Date >= initial && sr.Date <= final).Sum(sr => sr.Amount);
+
+        }
+
     }
+
 }
